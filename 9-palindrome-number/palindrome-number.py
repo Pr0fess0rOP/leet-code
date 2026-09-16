9c7e4aa6e979d660ada5266cd657de087bdaaf21
Pathik Viramgama <79077511+Pr0fess0rOP@users.noreply.github.com>
2026-09-15 22:10:22 -0700

    Time: 10 ms (41.11%) | Memory: 19.2 MB (55.11%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,7 +1,15 @@
-class Solution(object):
-    def isPalindrome(self, x):
-        x_char = list(str(x))
-        for i in range(len(x_char)//2):
-            if x_char[i] != x_char[-(i+1)]:
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        
+        x = str(x)
+
+        if len(x)%2 == 0:
+            n = len(x)/2
+        else:
+            n = (len(x)+1)/2
+
+        for i in range(int(n)):
+            if x[i] != x[-(i+1)]:
                 return False
+
         return True
