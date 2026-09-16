9559cc1c2a61002231462e22fef8e4abafe95a81
Pathik Viramgama <79077511+Pr0fess0rOP@users.noreply.github.com>
2026-09-15 22:12:23 -0700

    Time: 0 ms (100.00%) | Memory: 19.2 MB (59.14%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,16 +1,14 @@
-class Solution(object):
-    def romanToInt(self, s):
-        mother_teresa = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        julius_ceasar = list(s)
-        jesus_christ = 0
-
-        for i in range(len(julius_ceasar)-1):
-            if mother_teresa[julius_ceasar[i]] < mother_teresa[julius_ceasar[i+1]]:
-                jesus_christ -= mother_teresa[julius_ceasar[i]]
-            else:
-                jesus_christ += mother_teresa[julius_ceasar[i]]
-
-        jesus_christ += mother_teresa[julius_ceasar[-1]]
-
-        return jesus_christ
+class Solution:
+    def romanToInt(self, s: str) -> int:
         
+        symbolMap = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        nos = 0
+
+        for i in range(len(s)-1):
+
+            if symbolMap[s[i]] < symbolMap[s[i+1]]:
+                nos -= symbolMap[s[i]]
+            else:
+                nos += symbolMap[s[i]]
+
+        return (nos + symbolMap[s[-1]])
