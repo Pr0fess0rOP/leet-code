3e95e50c0614285918979b397ddfdb1d32d4beb5
Pathik Viramgama <79077511+Pr0fess0rOP@users.noreply.github.com>
2026-09-16 11:22:59 -0700

    Time: 0 ms (100.00%) | Memory: 19.2 MB (87.41%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -1,25 +1,37 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        # Corner Case
-        if x == 0:
-            return 0
-        
-        left = 1
-        right = x
-        ans = 0
-        while left <= right:
-            
-            mid = left + (right-left)//2
-            square = mid*mid
 
-            if square < x:
-                ans = mid
+        # #Brute
+        # if x == 1:
+        #     return 1
+        
+        # i = 0
+        # for i in range(x//2 + 1):
+        #     if i*i == x:
+        #         return i
+        #     if i*i < x and (i+1)*(i+1) > x:
+        #         return i
+        #     i = i +1
+
+        # Binary Search
+        left = 0
+        right = x
+        answer = 0
+
+        while left <= right:
+            mid = (left + right)//2
+        
+            if mid*mid == x:
+                return mid
+            
+            elif mid*mid > x:
+                right = mid -1 
+            
+            elif mid*mid < x:
+                answer = mid
                 left = mid + 1
 
-            elif square == x:
-                return mid
+        return answer
 
-            elif square > x:
-                right = mid - 1
-
-        return ans
+        
+                
