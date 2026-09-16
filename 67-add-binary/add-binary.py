190bb3912db24fb63f0a8e17006517afad56a7d4
Pathik Viramgama <79077511+Pr0fess0rOP@users.noreply.github.com>
2026-09-16 10:53:43 -0700

    Time: 3 ms (41.27%) | Memory: 19.6 MB (18.00%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -1,13 +1,11 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         a = list(a)
-        print(a)
         b = list(b)
         answer = []
         carry = 0
 
         for i in range(-1,-(min(len(a), len(b)) + 1), -1):
-            print("inside first half")
 
             addition = carry + int(a[i]) + int(b[i])
 
@@ -23,13 +21,8 @@ class Solution:
             if addition == 3:
                 answer.insert(0, 1)
                 carry = 1 
-            print(answer)
-            print(i)
-        
 
         for j in range(i- 1, -(max(len(a), len(b)) + 1), -1):
-            print("inside second half")
-            print(j)
             if len(a) > len(b):
                 addition = carry + int(a[j])
             else:
@@ -44,10 +37,8 @@ class Solution:
             if addition == 2:
                 answer.insert(0, 0)
                 carry = 1 
-            print(answer)
         
         if carry != 0:
-            print("inside carry loop")
             answer.insert(0,1)
             carry = 0
 
