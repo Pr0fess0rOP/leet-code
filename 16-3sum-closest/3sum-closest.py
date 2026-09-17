0ae7c220ac58828b0d3d1a4cf61d2d2c1ea92d89
Pathik Viramgama <79077511+Pr0fess0rOP@users.noreply.github.com>
2026-09-17 14:59:35 -0700

    Time: 383 ms (56.33%) | Memory: 19.2 MB (81.47%) - LeetSync

---
+++ 16-3sum-closest/3sum-closest.py
@@ -0,0 +1,41 @@
+class Solution:
+    def threeSumClosest(self, nums: list[int], target: int) -> int:
+
+        n = len(nums)
+        closest_sum = nums[0] + nums[1] + nums[2] 
+        
+        # Brute Force
+        # for i in range(n-2):  
+        #     for j in range(i+1, n-1):  
+        #         for k in range(j+1, n):
+        #             current_sum = nums[i] + nums[j] + nums[k] 
+        #             if abs(target - closest_sum) > abs(target - current_sum): 
+        #                 closest_sum = current_sum 
+
+        # 1 loop and 2 pointers
+        nums.sort()
+        for i in range(n-2):
+            left = i+1
+            right = n-1
+            while left < right:
+                current_sum = nums[i] + nums[left] + nums[right] 
+
+                # If we got the exact match, return that
+                if current_sum == target:
+                    return target
+    
+                if abs(target - closest_sum) > abs(target - current_sum):
+                    closest_sum = current_sum
+
+                # Decide the movement of left and right inside the subarray
+                
+                # if true, means my num combo needs a smaller number
+                # Since array is sorted, i can move "right" to a less index for a smaller number
+                if current_sum > target: 
+                    right -= 1
+                # since we already check 
+                # Since array is sorted, i can move "right" to a less index for a smaller number
+                else:
+                    left += 1
+
+        return closest_sum
