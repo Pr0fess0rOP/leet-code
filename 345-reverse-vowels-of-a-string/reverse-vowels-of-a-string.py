9c693ad679683d514e03d7add082274039c17790
Pathik Viramgama <79077511+Pr0fess0rOP@users.noreply.github.com>
2026-09-22 00:58:39 -0700

    Time: 7 ms (87.59%) | Memory: 20.7 MB (28.79%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
@@ -1,21 +1,44 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
-        s = list(s)
-        reversing_letters = []
-        reversing_index = []
-        answer = ""
-        vovel_dict = {'a': 'A', 'e': 'E', 'i': 'I', 'o': 'O', 'u': 'U'}
 
-        for idx, char in enumerate(s):
+        # two-phase / auxiliary-array approach
+        # O(n)
+        # s = list(s)
+        # reversing_letters = []
+        # reversing_index = []
+        # answer = ""
+        # vovel_dict = {'a': 'A', 'e': 'E', 'i': 'I', 'o': 'O', 'u': 'U'}
 
-            if char in vovel_dict.keys() or char in vovel_dict.values():
-                reversing_letters.append(char)
-                reversing_index.append(idx)
+        # for idx, char in enumerate(s):
 
-        for i in range(len(reversing_index)):
+        #     if char in vovel_dict.keys() or char in vovel_dict.values():
+        #         reversing_letters.append(char)
+        #         reversing_index.append(idx)
 
-            s[reversing_index[i]] = reversing_letters[len(reversing_index) - i - 1]
+        # for i in range(len(reversing_index)):
+
+        #     s[reversing_index[i]] = reversing_letters[len(reversing_index) - i - 1]
             
+        # return ''.join(s)
+
+
+        # Two Pointer Approach
+        s = list(s)
+        vowels = "aeiouAEIOU"
+        left = 0
+        right = len(s)-1
+
+        while left < right:
+            if s[left] in vowels:   
+                if s[right] in vowels:
+                    s[left], s[right] = s[right], s[left]
+                    left += 1
+                    right -= 1
+                else:
+                    right -= 1  
+            else:
+                left += 1
+
         return ''.join(s)
 
-        # print(reversing_letters, reversing_index)
+            
